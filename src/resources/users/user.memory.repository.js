09944546537c2842a080e114b6cac57b73508cd7
GBaykov <gleb.bykov99@gmail.com>
2021-12-03 20:03:32 +0300

    feat: add GET method of users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,38 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+// const {db} = require('../db/db')
+const users = [];
+const bords = [];
+const tasks = [];
 
-module.exports = { getAll };
+const db = [
+    users, 
+//= [
+//       {
+//           id: 1,
+//           name: 'Gleb',
+//           login: 'user',
+//   password: 'P@55w0rd'
+//       },
+//       {
+//         id: 2,
+//         name: 'fgh',
+//         login: 'fg',
+// password: '1234'
+//     }
+//   ],
+  bords,
+  tasks
+]
+const getAll = async () => 
+  // TODO: mock implementation. should be replaced during task development
+   db[0]
+
+;
+ 
+const getUser = async(id) => {
+  // console.log(id)
+  const user = await db[0].find(item => item.id === id);
+  return user;
+  }
+
+module.exports = { getAll, getUser };
+
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -8,4 +8,12 @@ router.route('/').get(async (req, res) => {
   res.json(users.map(User.toResponse));
 });
 
+
+router
+  .get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const user = await usersService.getUser(id);
+    res.json(User.toResponse(user));
+  })
+
 module.exports = router;
