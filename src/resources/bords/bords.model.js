ca1e60c89fbad3b07c711b74a8c619632c501e41
GBaykov <gleb.bykov99@gmail.com>
2021-12-05 15:32:50 +0300

    feat: add Columns model

--- src/resources/bords/bords.model.js
+++ src/resources/bords/bords.model.js
@@ -1,15 +1,23 @@
 // const uuid = require('uuid');
 const { v4: uuid } = require('uuid');
+const Column = require('./column.model');
 
 class Board {
   constructor({
     id = uuid(),
     title = 'title',
-    columns = 'columns',
+    columns,
   } = {}) {
     this.id = id;
     this.title = title;
     this.columns = columns;
+  }
+
+  static createColumns(columns) {
+    if (Array.isArray(columns)) {
+      return columns.map((col) => new Column({ ...col }));
+    }
+    return [new Column()];
   }
 
 }
